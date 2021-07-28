54a98a1b1a877a152488f89a885ac61016754aee
sivakesh <sivakesh@gmail.com>
2021-07-28 18:17:07 +0530

    next build

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1>Hello from V2</h1>
         </a>
       </header>
     </div>
